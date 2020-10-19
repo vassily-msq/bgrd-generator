aa0f0e579541aa443e410e4f1fdfb21f135f4a1a
VL <vasil.deville@gmail.com>
2020-10-19 18:07:50 +0300

    npm run

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Nodejs way 
+// var _ = require('lodash');
+
+// // Doesn't work as well
+// import { without } from 'lodash';
+// console.log(without);
+
+// Browserify
+var _ = require('lodash');
+console.log(_);
+
+var arr = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(arr, 3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
